9385ab86849f630f5582468ada9bc1dd3bfb9d31
David Morton <michaeldavidmorton@gmail.com>
2016-09-20 13:00:28 -0400

    Update view engine

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ mongoose.connect(config.mongoURI[app.settings.env], function(err, res) {
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
