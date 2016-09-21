b638c69ec89b7f6e5f54d665b0e5d64c2e4e9611
David Morton <michaeldavidmorton@gmail.com>
2016-09-21 15:48:11 -0400

    Switched to hbs for view engine

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var exphbs = require('express-handlebars');
 
 var routes = require('./routes/index');
 var api = require('./routes/api');
 var app = express();
+
 var config = require('./_config');
 
 // db connection
@@ -21,7 +23,8 @@ mongoose.connect(config.mongoURI[app.settings.env], function(err, res) {
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.engine('.hbs', exphbs({ defaultLayout: 'default', extname: '.hbs' }));
+app.set('view engine', '.hbs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
